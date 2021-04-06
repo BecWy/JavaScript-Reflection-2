596e490ffdd5589800e779f435e621d5f1c27250
Bec <70154628+BecWy@users.noreply.github.com>
2021-04-06 12:03:15 +0100

    Added JS event listeners

--- app.js
+++ app.js
@@ -5,24 +5,72 @@
 
 let url = "https://picsum.photos/300";
 let img = document.querySelector("#picsumImg");
+let img2 = document.querySelector("#gallery-img-1");
+let emailInput = document.querySelector("#email");
+const skip = document.querySelector("#skip");
+const save = document.querySelector("#save");
 
-fetch(url)
-.then(response => {
+let savedEmailAddress = "test";
+let newEmailAddress = emailInput.value;
+
+
+
+//EVENT LISTENERS ////////////////////////////////////////////////////////////////////////////////
+
+//Page loaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+    fetchFunction();
+})
+
+///Button event listeners
+
+//SKIP button
+//use the fetch function above to get a new image
+skip.addEventListener('click', () => {
+    fetchFunction();
+})
+
+
+//SAVE button
+save.addEventListener('click', () => {
+    if(newEmailAddress.toLowerCase() === savedEmailAddress.toLowerCase()) {
+        //push the image URL to the array that already exists for that email address
+        console.log("the email address is already stored");
+    } else {
+        //create a new array with this image URL saved to it.
+        console.log("the email address is new");
+    }
+})
+
+
+
+const fetchFunction = () => {
+    fetch(url)
+///get the data we need (aka the specific image's url) from the response
+.then(response => { 
     response.blob();
-    console.log(response);
+    //console.log(response);
     return response.url;
 })
+//set the image source to the url that comes back from the API. Also need to reuse this later to attach it to an email
 .then(data => {
     img.src = data;
-    console.log(data);
-    //need to push the data (aka the saved URL) to an array that is connected to an email address or something like that. Need to do something with it.
+    //console.log(data);
+    return data;
 })
+.then(data => {
+    console.log(data);
+    img2.style.backgroundImage = `url(${data})`;
+}) 
+//catch is what needs to happen if there is an error
 .catch(err => {
     //let nm = err.name;
     //let msg = err.message;
     //alert(`CATCH: ${nm} ${msg}`);
     alert("ERROR");
 });
+}
+
 
 
 
@@ -48,24 +96,12 @@ fetch(url)
 
 
 
-
-
-
-
-        // fetch(url)
-        // .then(response => {
-        //     response.json();
-        // })
-        // .then(data => {
-        //     let str = JSON.stringify(data, null, '\t');
-        //     document.querySelector("#output pre").textContent = str;
-        // })
-        // .catch(err => {
-        //     let nm = err.name;
-        //     let msg = err.message;
-        //     alert(`CATCH: ${nm} ${msg}`);
-        // });
-
+// let submittedData = {      //the data saved to each object
+//     name: nameInput.value, 
+//     email: emailInput.value, 
+//     phone: phoneInput.value,
+//     message: messageInput.value,
+// };
 
 
 
